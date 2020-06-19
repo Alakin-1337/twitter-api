939e4d3bcf51b7bb2982bfa8020273669d024ace
Alakin <alakin@free.fr>
2020-06-19 12:30:22 +0200

    final Twitter

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,22 @@
-"""
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-
-    from .main.controllers import main
-    app.register_blueprint(main)
-
-    return app
-"""
-
 from flask import Flask
 from flask_restx import Api
 
-from .db import tweet_repository
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+#from .db import tweet_repository
 from .models import Tweet
-tweet_repository.add(Tweet("a first tweet"))
-tweet_repository.add(Tweet("a second tweet"))
+
+#tweet_repository.add(Tweet("a first tweet"))
+#tweet_repository.add(Tweet("a second tweet"))
 
 def create_app():
     app = Flask(__name__)
+
+    from config import Config
+    app.config.from_object(Config)
+    db.init_app(app)
 
     @app.route('/hello')
     def hello():
--- app/apis/tweets.py
+++ app/apis/tweets.py
@@ -1,10 +1,13 @@
 #from flask_restplus import Namespace, Resource, fields
 from flask_restx import Namespace, Resource, fields
-from app.db import tweet_repository
+from flask import abort
+#from app.db import tweet_repository
+from app import db
+from app.models import Tweet
 
 api = Namespace('tweets')
 
-
+"""
 #All models instantiated with model(), clone() and inherit() will be automatically documented in your Swagger specifications.
 model = api.model('Tweet', {
     'id': fields.Integer,
@@ -18,8 +21,110 @@ model = api.model('Tweet', {
 class TweetResource(Resource):
     @api.marshal_with(model)
     def get(self, id):
-        tweet = tweet_repository.get(id)
+        #tweet = tweet_repository.get(id)
+        tweet = db.session.query(Tweet).get(id)
         if tweet is None:
             api.abort(404)
         else:
             return tweet
+"""
+
+json_tweet = api.model('Tweet', {
+    'id': fields.Integer,
+    'text': fields.String,
+    'created_at': fields.DateTime
+})
+
+json_new_tweet = api.model('New tweet', {
+    'text': fields.String(required=True)
+})
+
+@api.route('/<int:id>')
+@api.response(404, 'Tweet not found')
+@api.param('id', 'The tweet unique identifier')
+class TweetResource(Resource):
+    @api.marshal_with(json_tweet)
+    def get(self, id):
+        tweet = db.session.query(Tweet).get(id)
+        if tweet is None:
+            api.abort(404, "Tweet {} doesn't exist".format(id))
+        else:
+            return tweet
+
+    @api.marshal_with(json_tweet, code=200)
+    @api.expect(json_new_tweet, validate=True)
+    def patch(self, id):
+        tweet = db.session.query(Tweet).get(id)
+        if tweet is None:
+            api.abort(404, "Tweet {} doesn't exist".format(id))
+        else:
+            tweet.text = api.payload["text"]
+            return tweet
+
+    def delete(self, id):
+        tweet = db.session.query(Tweet).get(id)
+        if tweet is None:
+            api.abort(404, "Tweet {} doesn't exist".format(id))
+        else:
+            db.session.delete(tweet)
+            db.session.commit()
+            return None
+
+@api.route('')
+@api.response(422, 'Invalid tweet')
+class TweetsResource(Resource):
+    @api.marshal_with(json_tweet, code=201)
+    @api.expect(json_new_tweet, validate=True)
+    def post(self):
+        text = api.payload["text"]
+        if len(text) > 0:
+            tweet = Tweet(text=text)
+            db.session.add(tweet)
+            db.session.commit()
+            return tweet, 201
+        else:
+            return abort(422, "Tweet text can't be empty")
+"""
+class TweetResource(Resource):
+    @api.marshal_with(json_tweet)
+    def get(self, id):
+        #tweet = tweet_repository.get(id)
+        #tweet = db.session.query(Tweet).get(id)
+        tweet = db.session.query(Tweet).get(1)
+        if tweet is None:
+            api.abort(404, "Tweet {} doesn't exist".format(id))
+        else:
+            return tweet
+
+    @api.marshal_with(json_tweet, code=200)
+    @api.expect(json_new_tweet, validate=True)
+    def patch(self, id):
+        tweet = tweet_repository.get(id)
+        if tweet is None:
+            api.abort(404, "Tweet {} doesn't exist".format(id))
+        else:
+            tweet.text = api.payload["text"]
+            return tweet
+
+    def delete(self, id):
+        tweet = tweet_repository.get(id)
+        if tweet is None:
+            api.abort(404, "Tweet {} doesn't exist".format(id))
+        else:
+            tweet_repository.remove(id)
+            return None
+
+@api.route('')
+@api.response(422, 'Invalid tweet')
+class TweetsResource(Resource):
+    @api.marshal_with(json_tweet, code=201)
+    @api.expect(json_new_tweet, validate=True)
+    def post(self):
+        text = api.payload["text"]
+        if len(text) > 0:
+            tweet = Tweet(text)
+            tweet_repository.add(tweet)
+            return tweet, 201
+        else:
+            return abort(422, "Tweet text can't be empty")
+"""
